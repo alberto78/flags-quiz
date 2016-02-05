759525f8ba7a3cbb4a79f014339a640963b2bddb
Alberto Chovares <alberto.chovares@gmail.com>
2016-02-05 20:28:53 +0100

    (refactor): controllerAs and avoid

--- src/lib/controller.js
+++ src/lib/controller.js
@@ -1,22 +1,33 @@
 class MainController {
-    constructor($scope, mainService) {
-        this.$scope = $scope;
+    constructor(mainService) {
+        this.mainService = mainService;
 
-        $scope.version = '0.1';
+        this.version = '0.1';
+        this.level = 1;
+        this.data = null;
+
+        var parent = this;
 
         mainService.load().then(function(data){
-
-            $scope.level_changed = function() {
-                var flag = mainService.get_random_flag(data, $scope.level);
-                $scope.random_flag = flag.path;
-            }
-
-            $scope.levels = mainService.get_available_levels(data);
-            $scope.level = $scope.levels.length ? $scope.levels[0] : 0;
-
-            var flag = mainService.get_random_flag(data, $scope.level);
-            $scope.random_flag = flag.path;
+            parent.data = data;
+            parent.init();
         });
+    }
+
+    setup_flag() {
+        var flag = this.mainService.get_random_flag(this.data, this.level);
+        this.random_flag = flag.path;
+    }
+
+    level_changed() {
+        this.setup_flag();
+    }
+
+    init() {
+        this.levels = this.mainService.get_available_levels(this.data);
+        this.level = this.levels.length ? this.levels[0] : 0;
+
+        this.setup_flag();
     }
 }
 
